3bffb7be81bd769a424f973650c9ade8f467853b
Kasper Lin <kasperlxk@outlook.com>
2019-02-24 22:25:50 +0800

    Done with neutralization description.

--- H3 Codes/Data Neutralization.py
+++ H3 Codes/Data Neutralization.py
@@ -28,10 +28,32 @@ factor_list = [
     # "underlyinghisvol_90d", 
     # "tech_turnoverrate20", 
     # "tech_turnoverrate60", 
-    # "tech_turnoverrate120", 
     # "val_lnmv"
-    # The last 5 data haven't been downloaded yet for quota exceeded.
+    # The last 4 data haven't been downloaded yet for quota exceeded.
 ]
+
+#%%
+def get_data(factor_name): # get data from disk.
+    '''
+    Parameter:
+        factor_name: name of factors in Wind. (str)
+    Return:
+        forward-filled factor data. (pd.DataFrame)
+            index: months. (np.int64)
+            columns: stocks code list. (str)
+    '''
+    data = pd.read_csv(
+        open(
+            # Extract raw data.
+            path + "\\H3 Data\\Raw Data\\" + factor_name + ".csv", 
+            'r', # read-only mode for data protection.
+            encoding = "utf-8"
+        ), 
+        index_col = [0]
+    )
+    # Forward-fill nan to make quarter report fill the month.
+    data.fillna(method = 'ffill', inplace = True) 
+    return data
 
 #%%
 def get_processed_data(factor_name): # get data from disk.
@@ -55,6 +77,62 @@ def get_processed_data(factor_name): # get data from disk.
     return data
 
 #%%
-get_processed_data("roe_ttm").head()
+def industry_comparison(factor_name = "pb_lyr"):
+    # All industry in HS300.
+    sw_industry_data = get_processed_data("industry_sw")
+    sw_industry_list = list(sw_industry_data.iloc[:, 0].unique())
+    # Use certain factor data for comparison example between industry.
+    compare_data = get_data(factor_name)
+    compare_industry = pd.DataFrame(
+        index = sw_industry_list, 
+        columns = [factor_name]
+    )
+    for industry in sw_industry_list:
+        industry_stock_code_list = list(sw_industry_data[
+            sw_industry_data.iloc[:, 0] == industry
+        ].index)
+        # Some industry is not in HS300. 
+        try:
+            industry_data = compare_data[industry_stock_code_list]
+            compare_industry.loc[
+                industry, factor_name
+            ] = np.mean(np.mean(industry_data))
+        except:
+            continue
+    compare_industry.dropna(inplace = True)
+    return compare_industry
+
+#%%
+def plot_industry_comparison():
+    significant_comparison_industry_list = [
+        "pcf_ncf_ttm", 
+        "yoyprofit", 
+        "yoyroe", 
+        "invturn"
+    ]
+    plt.figure(figsize = (21, 18))
+    for i in range(len(significant_comparison_industry_list)):
+        plot_data = industry_comparison(
+            significant_comparison_industry_list[i]
+        )
+        plt.subplot(int("22" + str(i+1)))
+        sns.barplot(
+            x = plot_data.index, 
+            y = significant_comparison_industry_list[i], 
+            data = plot_data
+        )
+        plt.xticks(rotation = 60)
+        plt.title(
+            significant_comparison_industry_list[i], 
+            fontsize = 21
+        )
+    plt.suptitle(
+        "沪深300中不同行业部分因子平均值比较", 
+        fontsize = 36
+    )
+    plt.savefig(path + "\\H3 Plots\\Industry Comparison.png")
+
+#%%
+plot_industry_comparison()
 
 #%%
